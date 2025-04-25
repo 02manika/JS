0d8a4a11a418ec9aad80cdd56db6dcc845393c0c
02manika <91953165+02manika@users.noreply.github.com>
2025-04-25 06:23:45 +0000

    conversion of datatypes

---
+++ basics/conversion.js
@@ -0,0 +1,16 @@
+let score = 33
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+
+let isLoggedIn = "manika"
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
+
+// 1=> true
+// 0=> false
+// ""=> false
+//"some value"=> true
+
