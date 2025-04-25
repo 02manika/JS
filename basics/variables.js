dea66d8b8a1381a04b9df70f84bc35570950ac17
02manika <91953165+02manika@users.noreply.github.com>
2025-04-25 04:23:26 +0000

    variables

---
+++ basics/variables.js
@@ -0,0 +1,17 @@
+const accId = 12345
+let accEmail ="manika@gmail.com"
+var accPassword = "1278"
+accCity = "Jaipur"
+
+//accId = 2
+
+// console.log(accId);   // not allowed
+
+console.log(accId)
+
+accEmail = "agarwal@gmail.com"
+accPassword = "9876"
+accCity = "Banglore"
+
+console.table([accEmail, accId, accPassword, accCity])
+
