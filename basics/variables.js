d51d6c9f7fb952d67600c4c661198264d11f4d21
02manika <91953165+02manika@users.noreply.github.com>
2025-04-25 04:27:28 +0000

    variables

--- basics/variables.js
+++ basics/variables.js
@@ -1,7 +1,12 @@
 const accId = 12345
 let accEmail ="manika@gmail.com"
-var accPassword = "1278"
+var accPassword = "1278"          
+/* 
+     prefer not to use var
+     beacuse of issue in block scope and functional scope
+*/
 accCity = "Jaipur"
+let accState
 
 //accId = 2
 
@@ -13,5 +18,5 @@ accEmail = "agarwal@gmail.com"
 accPassword = "9876"
 accCity = "Banglore"
 
-console.table([accEmail, accId, accPassword, accCity])
+console.table([accEmail, accId, accPassword, accCity, accState])
 
