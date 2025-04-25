0b02975854c33a64ca979a0112fe6e4c465ef7ca
02manika <91953165+02manika@users.noreply.github.com>
2025-04-25 04:39:31 +0000

    datatypes commit

---
+++ basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict"   // treat all JS code as newer version 
+
+// alert(3+3)    
+// we are using nodejs not browser
+
+console.log("manika")
+let name = "manika"
+let age = 18
+let islogg = false
+
+console.table([name, age, islogg])
+
+// number => 2^53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => uniqueness find krne ke lie 
+
+//object => derived data type
+
+console.log(typeof null)    // object
+console.log(typeof undefined)    // undefined
