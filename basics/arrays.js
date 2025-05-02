54ce3c98d42ce428a6ed2ded6511fb785df4de9b
02manika <91953165+02manika@users.noreply.github.com>
2025-05-02 16:47:56 +0000

    arrays commit

---
+++ basics/arrays.js
@@ -0,0 +1,18 @@
+let marks =[98,67,34,56]
+for(let i=0;i<marks.length;i++)     // printing of array
+{
+    console.log(marks[i])
+}
+
+// arrays methods 
+
+let num=[1,2,45,34,65]
+/*
+let b = num.toString()
+console.log(b)
+console.log(num.pop())
+delete num[3]
+console.log(num)
+*/
+console.log(num.sort())
+console.log(num.reverse())
